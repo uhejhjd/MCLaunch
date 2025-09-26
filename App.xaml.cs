36926892fd305cfeadc7a22f3950444940b5cad7
uhejhjd <mc88880408@163.com>
2025-09-26 21:00:50 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Flurl;
+using Flurl.Http;
+using MinecraftLaunch;
+using MinecraftLaunch.Base.Enums;
+using MinecraftLaunch.Base.Models;
+using MinecraftLaunch.Base.Models.Game;
+using MinecraftLaunch.Base.Models.Network;
+using MinecraftLaunch.Components.Authenticator;
+using MinecraftLaunch.Components.Downloader;
+using MinecraftLaunch.Components.Installer;
+using MinecraftLaunch.Components.Logging;
+using MinecraftLaunch.Components.Parser;
+using MinecraftLaunch.Extensions;
+using MinecraftLaunch.Launch;
+using MinecraftLaunch.Utilities;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using Panuon.WPF.UI;
+InitializeHelper.Initialize(settings =>
+{
+    settings.MaxThread = 256;
+    settings.MaxFragment = 128;
+    settings.MaxRetryCount = 4;
+    settings.IsEnableMirror = false;
+    settings.IsEnableFragment = false;
+    //settings.CurseForgeApiKey = "Your Curseforge API";
+    settings.UserAgent = "MLTest/1.0";
+});
+namespace MCLaunch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Panuon.WPF.UI;
+
+namespace MCLaunch;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : WindowX
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
